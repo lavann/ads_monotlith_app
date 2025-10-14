6e3faf9f0cc983c5fd8e76522f6a55018b8a54ee
lavann <lavann@gmail.com>
2025-10-14 13:34:13 +0100

    Initial commit + models

---
+++ Models/Cart.cs
@@ -0,0 +1,25 @@
+﻿namespace RetailMonolith.Models
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+
+        public string CustomerId { get; set; } = "guest";
+
+        public List<CartLine> Lines { get; set; } = new();
+    }
+
+    public class CartLine
+    {
+        public int Id { get; set; }
+        public int CartId { get; set; }
+
+        public Cart? Cart { get; set; }
+
+        public string Sku { get; set; }
+
+        public decimal UnitPrice { get; set; }
+        public int Quantity { get; set; }
+
+    } 
+}
