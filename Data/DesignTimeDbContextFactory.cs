4e9ecb4697c8dfa12e8d4c2ddb81d520ed69a67d
lavann <lavann@gmail.com>
2025-10-19 19:56:36 +0100

    update .gitignore

---
+++ Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace RetailMonolith.Data
+{
+    public class DesignTimeDbContextFactory: IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlServer(
+                "Server=(localdb)\\MSSQLLocalDB;Database=RetailMonolith;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
