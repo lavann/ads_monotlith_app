9c05d7df9ced86c6b9b702807bf9f3c655104d83
lavann <lavann@gmail.com>
2025-10-19 21:23:30 +0100

    feat(core): add cart. TODO - Cart Orchestration Logic, product list, art view, no meaningful data to persist. Create or find cart for current user, add a cartline to product, save in db. navigate to /cart to view items. Add ICartService with implementation to broker between products and cart

--- Pages/Cart/Index.cshtml.cs
+++ Pages/Cart/Index.cshtml.cs
@@ -1,12 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RetailMonolith.Data;
 
 namespace RetailMonolith.Pages.Cart
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
-        }
+       
+        private readonly AppDbContext _context;
+        public IndexModel(AppDbContext context) => _context = context;
+
+
+
+        //map the cart state in memory without mapping directly to the database
+        // Each line represents an item in the cart with its name, quantity, and price
+        //is this a good way to represent a cart in memory?
+        public List<(string Name, int Quantity, decimal Price)> Lines { get; set; } = new(); 
+
+        public decimal Total => Lines.Sum(line => line.Price * line.Quantity);
+
+
+
+
     }
 }
